839df21c4e2533a2f3e66d86fff67b5f4da8e941
CodeRaurus <coderaurus@gmail.com>
2025-04-06 18:29:40 +0300

    Change bucket stacking

--- scripts/buckets.gd
+++ scripts/buckets.gd
@@ -4,18 +4,19 @@ class_name Buckets
 const BUCKET_SPRITE = preload("res://scenes/bucket_sprite.tscn")
 
 var stack_direction: Vector2
-var stack_distance := 16
+var stack_distance := 2
 
 func _ready() -> void:
 	if has_buckets():
 		var b: Sprite2D = get_child(0)
-		stack_direction = global_position.direction_to(b.global_position).normalized()
+		#stack_direction = global_position.direction_to(b.global_position).normalized()
+		#stack_distance = global_position.distance_to(b.global_position)
 
 func has_buckets() -> bool:
 	return get_child_count() > 0
 
 func add_bucket() -> void:
-	var new_pos: Vector2 = stack_direction * stack_distance * get_child_count()
+	var new_pos: Vector2 = Vector2(-8,-8) + Vector2(-1,-1) * stack_distance * (get_child_count()+1)
 	var new_b = BUCKET_SPRITE.instantiate()
 	add_child(new_b)
 	#new_b.rotation = new_rotation if new_rotation else deg_to_rad(-25)
